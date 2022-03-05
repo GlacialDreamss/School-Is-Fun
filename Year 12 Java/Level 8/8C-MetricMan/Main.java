222676772b8e41b0cb48f82b3a5c3c888d6aaa4a
GlacialDreamss <danieladetokunbo05@gmail.com>
2022-03-05 19:50:33 +0000

    Update Main.java

--- Year 12 Java/Level 8/8C-MetricMan/Main.java
+++ Year 12 Java/Level 8/8C-MetricMan/Main.java
@@ -6,33 +6,38 @@ class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What would you like to do:\nA) Fahrenheit to Celsius\nB) Celcius to Fahrenheit\nC) Miles to Kilometers\nD) Kilometers to Miles\nE) Inches to Centimeters\nF) Centimeters to Inches\nX) Quit the program");    
+        System.out.println("What would you like to do:\nA) Fahrenheit to Celsius\nB) Celcius to Fahrenheit\nC) Miles to Kilometers\nD) Kilometers to Miles\nE) Inches to Centimeters\nF) Centimeters to Inches\nX) Quit the program");    
         String choice = input.nextLine();
         
         if (choice.equals("A")) {
             System.out.println("Enter the temperature (C)");
-            int temp = input.nextLine();
-            convertFtoC(temp);
+            double temp = Integer.parseInt(input.nextLine());
+            System.out.print(convertFtoC(temp));
         }
         else if (choice.equals("B")) {
             System.out.println("Enter the temperature (F)");
-            int temp = input.nextLine();            
+            double temp = Integer.parseInt(input.nextLine());    
+            System.out.print(convertCtoF(temp));                    
         }
         else if (choice.equals("C")) {
             System.out.println("Enter the distance (M)");
-            int distance = input.nextLine(); 
+            double distance = Integer.parseInt(input.nextLine()); 
+            System.out.print(convertMtoK(distance));
         }
         else if (choice.equals("D")) {
             System.out.println("Enter the distance (K)");
-            int distance = input.nextLine(); 
+            double distance = Integer.parseInt(input.nextLine());
+            System.out.print(convertKtoM(distance)); 
         }
         else if (choice.equals("E")) {
             System.out.println("Enter the length (I)");
-            int length = input.nextLine(); 
+            double length = Integer.parseInt(input.nextLine());
+            System.out.print(convertItoC(length));  
         }
         else if (choice.equals("F")) {
-            System.out.println("Enter the length (Cm)");
-            int length = input.nextLine(); 
+            System.out.println("Enter the length (C)");
+            double length = Integer.parseInt(input.nextLine());
+            System.out.print(convertCtoI(length));   
         }
         else if (choice.equals("X")) {
 
@@ -42,25 +47,25 @@ class Main {
         }
     }
 
-    public static void convertFtoC(int[] temp) {
-        return (length*3.15);
+    public static double convertFtoC(double temp) {
+        return ((temp - 32) * 5/9);
     }
-    public static void convertCtoF(int[] temp) {
-        
-    }
-
-    public static void convertMtoK(int[] distance) {
-        
-    }
-    public static void convertKtoM(int[] distance) {
-        
+    public static double convertCtoF(double temp) {
+        return ((temp * 9/5) + 32);
     }
 
-    public static void convertItoC(int[] length) {
-        
+    public static double convertMtoK(double distance) {
+        return (distance*1.609);        
     }
-    public static void convertCtoI(int[] length) {
-        
+    public static double convertKtoM(double distance) {
+        return (distance/1.609);          
+    }
+
+    public static double convertItoC(double length) {
+        return (length*2.54);         
+    }
+    public static double convertCtoI(double length) {
+        return (length/2.54); 
     }
 }
 
