d40a7bccb13baf03c31e4a6f087ab0f5a5a7f31e
GlacialDreamss <danieladetokunbo05@gmail.com>
2022-05-14 12:31:55 +0100

    Restart VScode...

---
+++ Year 12 Java/Object Oriented Programming/Definition Examples/accessor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class accessor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = input.nextLine();
+
+        public int printInput(){
+            return value
+        }
+    }    
+}
